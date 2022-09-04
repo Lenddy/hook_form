918f22d961ef457dd0d23badd2ec2f32e549efa3
Lenddy Morales <leme2003@gmail.com>
2022-09-04 00:53:17 -0400

    hook form

---
+++ src/components/Form.jsx
@@ -0,0 +1,69 @@
+import React ,{useState}  from "react";
+
+const Form = (props) =>{
+    const {f_name,l_name, email, password,confirm_password} = props
+    const [userF_name,setF_name] = useState(f_name); // need to change to an empty string
+    const [userL_name,setL_name] = useState(l_name);
+    const [userEmail,setEmail] = useState(email);
+    const [userPassword,setPassword] = useState(password);
+    const [userConfirm_password,setConfirm_password] = useState(confirm_password);
+    // const [image,setImage] = useState("");
+    // const createUser = (e) =>{
+    //     e.preventDefault();
+    //     const newUser = {
+    //     userF_name,
+    //     userL_name,
+    //     userEmail,
+    //     userPassword,
+    //     userConfirm_password};
+    //     console.log("new user",newUser);
+    // }
+    return(
+    <div className="Form" >
+        
+        <h3>fill this form</h3>
+        <form /*onSubmit={createUser}*/>
+            <div>
+                <label>first name</label>
+                <input onChange={(e) => {setF_name(e.target.value)}} type="text" name="" id="" />
+            </div>
+            <div>
+                <label>Last name</label>
+                <input onChange={(e) => {setL_name(e.target.value)}} type="text" name="" id="" />
+            </div>
+            <div>
+                <label>Email</label>
+                <input onChange={(e)=> {setEmail(e.target.value)}} type="text" name="" id="" />
+            </div>
+            <div>
+                <label>Password</label>
+                <input onChange={(e) =>{setPassword(e.target.value)} } type="password" name="" id="" />
+            </div>
+            <div>
+            <label>confirm Password</label>
+                <input onChange={(e)=>{setConfirm_password(e.target.value)}} type="password" name="" id="" />
+            </div>
+            {/* <div>
+                <label htmlFor=""> image url</label>
+                <input onChange={(e) => {setImage(e.target.value)}}  type="text" name="" id="" />
+            </div> */}
+            <div>
+                <input type="submit" />
+            </div>
+        </form>
+
+        <div>
+        <p>your info:</p>
+        <p>first name: {userF_name} </p>
+        <p>last name: {userL_name} </p>
+        <p>email: {userEmail}</p>
+        <p>Password: {userPassword}</p>
+        <p>confirm Password: {userConfirm_password}</p>
+        {/* <img src={image} heigh= "200px" /> */}
+        </div>
+    </div>
+    )
+
+}
+
+export default Form;
