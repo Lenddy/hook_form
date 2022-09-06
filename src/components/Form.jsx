8a9abd4b656bcd45fd83e8b7cb057771df0b1069
Lenddy Morales <leme2003@gmail.com>
2022-09-06 17:01:19 -0400

    hook form

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -8,15 +8,29 @@ const Form = (props) =>{
     const [userPassword,setPassword] = useState(password);
     const [userConfirm_password,setConfirm_password] = useState(confirm_password);
     // const [image,setImage] = useState("");
-    // const createUser = (e) =>{
-    //     e.preventDefault();
-    //     const newUser = {
-    //     userF_name,
-    //     userL_name,
-    //     userEmail,
-    //     userPassword,
-    //     userConfirm_password};
-    //     console.log("new user",newUser);
+    const createUser = (e) =>{
+        e.preventDefault();
+        const newUser = {
+        userF_name,
+        userL_name,
+        userEmail,
+        userPassword,
+        userConfirm_password};
+        console.log("new user",newUser);
+    }
+    // const showError =() =>{
+    //     if(userF_name.length == null)
+    //     {
+    //         return null
+    //     }
+    //     else if(userF_name.length > 3){
+    //         return (
+    //                 <p>name must be at lest 3 character long</p>
+    //                 )
+    //     }
+    //     else{
+    //         return null
+    //     }
     // }
     return(
     <div className="Form" >
@@ -26,22 +40,35 @@ const Form = (props) =>{
             <div>
                 <label>first name</label>
                 <input onChange={(e) => {setF_name(e.target.value)}} type="text" name="" id="" />
+                {userF_name.length < 3?<p>first name must be longer than 3 characters </p>
+                : null
+                }
+                
             </div>
             <div>
                 <label>Last name</label>
                 <input onChange={(e) => {setL_name(e.target.value)}} type="text" name="" id="" />
+                {userL_name.length < 3?<p>last name must be longer than 3 characters</p>: null
+                }
             </div>
             <div>
                 <label>Email</label>
                 <input onChange={(e)=> {setEmail(e.target.value)}} type="text" name="" id="" />
+                {
+                    userEmail.length < 5?<p>email must be a least 5 character long</p>: null
+                }
             </div>
             <div>
                 <label>Password</label>
                 <input onChange={(e) =>{setPassword(e.target.value)} } type="password" name="" id="" />
+                {userPassword.length < 8?<p>password must be longer than 8 characters</p>: null
+                }
             </div>
             <div>
             <label>confirm Password</label>
                 <input onChange={(e)=>{setConfirm_password(e.target.value)}} type="password" name="" id="" />
+                {userConfirm_password !== userPassword?<p>password must match </p>: null
+                }
             </div>
             {/* <div>
                 <label htmlFor=""> image url</label>
